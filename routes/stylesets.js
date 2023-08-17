673289cb3ca7090a856072bbc859131b99996cc3
Alex Stepanov <stepanov.hw@gmail.com>
2023-08-17 14:10:39 -0600

    code format routes/

--- routes/stylesets.js
+++ routes/stylesets.js
@@ -1,15 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const stylesetsCtrl = require('../controllers/stylesets');
-const ensureLoggedIn = require('../config/ensureLoggedIn');
+const stylesetsCtrl = require("../controllers/stylesets");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 // create (generate) a styleset
-router.post('/themes/:id/stylesets', ensureLoggedIn, stylesetsCtrl.create);
+router.post("/themes/:id/stylesets", ensureLoggedIn, stylesetsCtrl.create);
 
 // delete a styleset
-router.delete('/themes/:tid/stylesets/:ssid', ensureLoggedIn, stylesetsCtrl.delete);
+router.delete(
+  "/themes/:tid/stylesets/:ssid",
+  ensureLoggedIn,
+  stylesetsCtrl.delete
+);
 
 // update a styleset
-router.put('/themes/:tid/stylesets/:ssid', ensureLoggedIn, stylesetsCtrl.update);
+router.put(
+  "/themes/:tid/stylesets/:ssid",
+  ensureLoggedIn,
+  stylesetsCtrl.update
+);
 
 module.exports = router;
